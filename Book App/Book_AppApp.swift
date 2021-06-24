acfb239617c774a4e92dfa4e079285a057ac85f8
Lucas Van Tilburg <lucasvantilburg101202@gmail.com>
2021-06-25 08:35:01 +1000

    Initial Commit

---
+++ Book App/Book_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_AppApp.swift
+//  Book App
+//
+//  Created by Lucas Van Tilburg on 25/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Book_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
